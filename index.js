24896aa5a93b16f6bed39cc564e56329b48b989a
Israel-machine <israel.diaz1701@gmail.com>
2026-02-18 00:20:40 -0800

    Event Listeners Lab

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 // Handle Button Clicks
-
+const body = document.querySelector("body");
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
+  body.style.backgroundColor = 'rgb(255, 0, 0)'
   // Implement the function to change background color
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
+  body.style.backgroundColor = ''
   // Implement the function to reset background color
 }
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
+
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  const textInput = document.getElementById("keyPressDisplay")
+  textInput.textContent = 'Key pressed: ' + event.key;
 }
 
 // Process Text Input
 
 // Function to display user input in real-time
 function displayUserInput() {
+  const textInput = document.getElementById("textInput")
+  const display = document.getElementById("textInputDisplay")
+  display.textContent = 'You typed: '+ textInput.value;
   // Implement the function to display user input
 }
 
 // Attach Event Listeners
 function setupEventListeners() {
-// Attach event listener to change background color when the button is clicked
+  // Attach event listener to change background color when the button is clicked
   document
     .getElementById('changeColorButton')
     .addEventListener('click', changeBackgroundColor)
